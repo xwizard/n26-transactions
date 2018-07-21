fabecc5250014e4ca241b19c2976692eedd9daf7
xwizard <xwizard@server>
2018-07-21 16:14:52 +0200

    Beerter use spring-boot generator :)

---
+++ src/main/java/com/example/n26/n26transactions/N26TransactionsApplication.java
@@ -0,0 +1,12 @@
+package com.example.n26.n26transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N26TransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N26TransactionsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/n26/n26transactions/N26TransactionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.n26.n26transactions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class N26TransactionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

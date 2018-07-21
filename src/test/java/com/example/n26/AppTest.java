1b812871ff5c9822c96892d30ac7cba645f588b5
xwizard <xwizard@server>
2018-07-21 16:00:15 +0200

    Archetype generated code

---
+++ src/test/java/com/example/n26/AppTest.java
@@ -0,0 +1,20 @@
+package com.example.n26;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
